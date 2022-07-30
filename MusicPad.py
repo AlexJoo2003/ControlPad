7fce8f5bcccbd1edd0f15db4a315bc27b0120322
AlexJoo2003 <alexanderalnoumeh@gmail.com>
2022-07-30 17:03:23 +0200

    Integrated the launchpad and made button ligths responsive. Also created the Meta Buttons and the Mk1 default setup

--- MusicPad.py
+++ MusicPad.py
@@ -3,6 +3,16 @@ from os.path import exists  # To check if the config file exists
 import json # To read and write the config file
 from time import sleep
 from pprint import pprint
+import pyperclip
+import os
+import pyautogui
+try:
+    import launchpad_py as launchpad
+except ImportError:
+    try:
+        import launchpad
+    except ImportError:
+        sys.exit("error loading launchpad.py")
 
 #TODO: Detect launchpad button presses
 #TODO: Manipulate launchpad button lights
@@ -13,18 +23,37 @@ from pprint import pprint
 
 pad = None  # Global MusicPad object
 lp = None   # Global Launchpad object
+if launchpad.Launchpad().Check(0):
+    lp = launchpad.Launchpad()
+    if lp.Open(0):
+        print("Launchpad Mk1/S/Mini")
+    lp.Reset()
+else:
+    print("Launchpad not available")
+    exit()
 
 class Button:
     def __init__(self, position, **args):
         self.position = position
         self.args = args
         self.color = "white"
-    
+        self.changeColor(self.color)
+
     def run(self):
         print("Button pressed")
     
-    def changeColor(self, color):   
+    def changeColor(self, color):
         self.color = color
+        if color == "green":
+            lp.LedCtrlXY(self.position[0], self.position[1], 0, 1)
+        elif color == "orange":
+            lp.LedCtrlXY(self.position[0], self.position[1], 1, 1)
+        elif color == "red":
+            lp.LedCtrlXY(self.position[0], self.position[1], 1, 0)
+        elif color == "white":
+            lp.LedCtrlXY(self.position[0], self.position[1], 0, 0)
+        print("Changed", self.position, " color to ", self.color)
+        
         # lp.position color etc etc...
 
 class PageButton(Button):
@@ -32,6 +61,11 @@ class PageButton(Button):
         Button.__init__(self, position, **args)
         self.page = position[0]
         self.selected = False
+        if pad.current_page == self.page:
+            self.color = "green"
+        else:
+            self.color = "white"
+        self.changeColor(self.color)
     
     def run(self):
         pad.changePage(self.page)
@@ -40,6 +74,68 @@ class PageButton(Button):
 class MetaButton(Button):
     def __init__(self, position, **args):
         Button.__init__(self, position, **args)
+        self.function_name = args["function"]
+        self.function = getattr(self, self.function_name)
+        self.color = "green"
+        if self.function_name == "exit":
+            self.color = "red"
+        elif "mode_toggle" in self.function_name:
+            self.color = "orange"
+        self.changeColor(self.color)
+    
+    def run(self):
+        self.function()
+    
+    def change_volume(self, volume):
+        current_volume = mixer.music.get_volume()
+        new_volume = current_volume + volume
+        
+        if new_volume <= 0:
+            new_volume = 0
+        elif new_volume >= 1:
+            new_volume = 1
+        
+        print("changed volume to ", new_volume)
+        mixer.music.set_volume(new_volume)
+    
+    def lower_volume(self):
+        self.change_volume(-0.1)
+    
+    def raise_volume(self):
+        self.change_volume(0.1)
+    
+    def stop_music(self):
+        mixer.music.stop()
+        print("stopped the music")
+    
+    def toggle_pause_music(self):
+        if mixer.music.get_busy():
+            mixer.music.pause()
+        else:
+            mixer.music.unpause()
+
+    def create_mode_toggle(self):
+        if pad.create_mode:
+            self.changeColor("orange")
+        else:
+            self.changeColor("green")
+            
+        pad.create_mode = not pad.create_mode
+
+    def delete_mode_toggle(self):
+        if pad.delete_mode:
+            self.changeColor("orange")
+        else:
+            self.changeColor("red")
+
+        pad.delete_mode = not pad.delete_mode
+
+    def exit(self):
+        print("exiting")
+        mixer.music.stop()
+        lp.Reset()
+        lp.Close()
+        exit()
 
 class FunctionButton(Button):
     def __init__(self, position, **args):
@@ -53,37 +149,45 @@ class SoundButton(FunctionButton):
     def __init__(self, position, **args):
         FunctionButton.__init__(self, position, **args)
         self.path = args["path"]
-        self.in_action = False
+        self.color = "green"
+        self.changeColor(self.color)
 
     def run(self):
-        if self.in_action:
-            if mixer.music.get_busy():
-                mixer.music.pause()
-            else:
-                mixer.music.unpause()
-        else:
-            for button in pad.buttons:  # This loop makes this button the only button in action
-                if isinstance(button, SoundButton):
-                    if button != self:
-                        button.in_action = False
-            self.in_action = True
-            mixer.music.load(self.path)
-            mixer.music.play()
-            print("Playing Sound")
-    
+        mixer.music.load(self.path)
+        mixer.music.play()
+        print("Playing Sound")
+
+class HotKeyButton(FunctionButton):
+    def __init__(self, position, **args):
+        FunctionButton.__init__(self, position, **args)
+        self.keys = args["keys"]
+        self.color = "orange"
+        self.changeColor(self.color)
+
+    def run(self):
+        for key in self.keys:           # Press down the Keys
+            pyautogui.keyDown(key)
+        for key in self.keys:           # Release the Keys
+            pyautogui.keyUp(key)
+
+
 class MusicPad:
     def __init__(self):
-
         self.config_path = "./config.json"
         config = self.loadConfig()
+        self.current_page = config["current_page"]
 
         self.buttons = []
+        
+        self.create_mode = False
+        self.delete_mode = False
+
+    def createButtons(self):
+        config = self.loadConfig()
         for button in config["buttons"]:
             constructor = globals()[button["class"]]    # Finds the class from a string...
             instance = constructor(button["position"], **button["args"])  # then creates the object
             self.buttons.append(instance)
-        
-        self.current_page = config["current_page"]
 
     def loadConfig(self):
         print("Loading Config...")
@@ -122,10 +226,12 @@ class MusicPad:
         for button in self.buttons:
             if button.position == position:
                 if not isinstance(button, FunctionButton):         # return any non grid based button in the same position
+                    print("Found a non grid button at ", position)
                     return button
                 buttons.append(button)
         for button in buttons:
             if button.page == args["page"]:
+                print("Found a grid button at ", position)
                 return button                           # return the grid based button on the same page
 
     def createButton(self, position, button_class, **args):
@@ -140,6 +246,7 @@ class MusicPad:
 
     def deleteButton(self, position, **args):
         if button := self.searchButton(position, **args):
+            button.changeColor("white")
             self.buttons.remove(button)
             self.saveConfig()
             print("Deleted a button")
@@ -147,22 +254,59 @@ class MusicPad:
             print("Cannot delete a non-existing button")
 
     def pressButton(self, position):
+        self.delete_mode = False
+        self.create_mode = False
+        for button in pad.buttons:      # This turns off the delete_mode and create_mode if they are turned on
+            if isinstance(button, MetaButton) and hasattr(button, "function_name"):
+                if "mode_toggle" in button.function_name and button.color != "orange":
+                    button.changeColor("orange")
+
         if button := self.searchButton(position, page = self.current_page):
-            print("Pressing the button")
-            button.run()
+            if isinstance(button, FunctionButton) and self.delete_mode:
+                self.deleteButton(position, page = self.current_page)
+                for button in self.buttons:
+                    if hasattr(button, "function_name"):
+                        if button.function_name == "delete_mode_toggle":
+                            button.run()            # Turns off the delete mode and turns the button back to orange
+            else:
+                print("Pressing the button")
+                button.run()
         else:
-            print("Couldn't press the button, the button doesn't exist")
-            # Create a new button perhaps
-    
+            if self.create_mode:
+                path = pyperclip.paste()
+                if path.startswith('"') and path.endswith('"'):
+                    path = path[1:-1]   # remove the double quotes if the user used the copy function in the explorer
+                if os.path.exists(path):                             # Use the path for the song
+                    pad.createButton(position, SoundButton, path = path, page = self.current_page)
+                    for button in self.buttons:
+                        if hasattr(button, "function_name"):
+                            if button.function_name == "create_mode_toggle":
+                                button.run()            # Turns off the create mode and turns the button back to orange
+                else:
+                    print("Path doesn't exist", path)
+            else:
+                print("Couldn't press the button, the button doesn't exist")
+
     def changePage(self, page):
+        for button in self.buttons:     # Turns off all the buttons on the current page
+            if isinstance(button, FunctionButton):
+                if button.page == self.current_page:
+                    button.changeColor("white")
+
         if page != self.current_page:
-            self.current_page = page
             for button in self.buttons:
-                if isinstance(button, PageButton):
+                if isinstance(button, PageButton): # Turns the selected page green while every other page white
                     if button.page != page:
-                        button.changeColor("white")
+                        if button.color != "white":
+                            button.changeColor("white") 
                     else:
                         button.changeColor("green")
+                if isinstance(button, FunctionButton):  # Turns off all the buttons on the current page and turns on the buttons on the new page
+                    if button.page == self.current_page:
+                        button.changeColor("white")
+                    elif button.page == page:
+                        button.changeColor("green")
+            self.current_page = page
 
 def pause_test():
     pad.purgeConfig()
@@ -222,13 +366,54 @@ def page_test():
     input(">>> Done")
     pad.purgeConfig()
 
+def LaunchPadMk1_default_setup():
+
+    pad.deleteButton([0,0])
+    pad.deleteButton([1,0])
+    pad.deleteButton([2,0])
+    pad.deleteButton([3,0])
+    pad.deleteButton([4,0])
+    pad.deleteButton([5,0])
+    pad.deleteButton([6,0])
+    pad.deleteButton([7,0])
+    pad.deleteButton([8,1])
+    pad.deleteButton([8,2])
+    pad.deleteButton([8,3])
+    pad.deleteButton([8,5])
+    pad.deleteButton([8,6])
+    pad.deleteButton([8,8])
+
+    pad.createButton([0,0], PageButton)
+    pad.createButton([1,0], PageButton)
+    pad.createButton([2,0], PageButton)
+    pad.createButton([3,0], PageButton)
+    pad.createButton([4,0], PageButton)
+    pad.createButton([5,0], PageButton)
+    pad.createButton([6,0], PageButton)
+    pad.createButton([7,0], PageButton)
+    pad.createButton([8,1], MetaButton, function = "raise_volume")
+    pad.createButton([8,2], MetaButton, function = "lower_volume")
+    pad.createButton([8,3], MetaButton, function = "stop_music")
+    pad.createButton([8,5], MetaButton, function = "create_mode_toggle")
+    pad.createButton([8,6], MetaButton, function = "delete_mode_toggle")
+    pad.createButton([8,8], MetaButton, function = "exit")
+
 def main():
+
     mixer.init()                
     
     global pad
     pad = MusicPad()
+    pad.createButtons()
 
+    # LaunchPadMk1_default_setup()
     
+    while True:
+        buts = lp.ButtonStateXY()
+        if buts:
+            if not buts[-1]:
+                position = [buts[0], buts[1]]
+                pad.pressButton(position)
 
 
 if __name__ == "__main__":
